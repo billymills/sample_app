a26cca70edfd9e93578a9d56e19e08ac7068d640
Billy Mills <billymills@Billys-MacBook-Pro.local>
2013-02-06 20:48:07 -0500

    sample app passing tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,40 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
-    it "should have the contect 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')	
-    end
-  end
+  	describe "Home page" do
+    	it "should have the h1 'gigheap.com | Home'" do
+      		visit '/static_pages/home'
+      		page.should have_selector('h1', :text => 'gigheap.com | Home')	
+    	end
+		
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => 'gigheap.com | Home')
+		end
+  	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
+		it "should have the h1 'gigheap.com | Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'gigheap.com | Help')
+		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => 'gigheap.com | Help')
 		end
 	end
 
 	describe "About page" do
-		it "should have the content 'About'" do
+		it "should have the h1 'gigheap.com | About'" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1', :text => 'gigheap.com | About')
+		end
+
+		it "should have the title 'About'" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => 'gigheap.com | About')
 		end
 	end
-
 
 end
